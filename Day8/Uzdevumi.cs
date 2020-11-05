864f200683a654aa78604e477b3f9e54f4c4b4a7
Losstarot09 <lobanovs.arturs@gmail.com>
2020-11-05 19:35:38 +0200

    Da8 Task 1 complete

---
+++ Day8/Uzdevumi.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+
+namespace Day8
+{
+    class Uzdevumi
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            bool check = false;
+            while (!check)
+            {
+                Console.Write("Lūdzu. ievadiet veselo skaitļi 1 vai extra, lai izpildīt attiecīgo uzdevumu \n" +
+                    " (exit, lai izietu no programmas): ");
+                switch (Console.ReadLine().Trim())
+                {
+                    case "1":
+                        {
+                            Random random = new Random();
+                            int size = random.Next(6) + 5;
+                            int[] intArray = new int[size];
+                            for (int i = 0; i < size; i++)
+                            {
+                                intArray[i] = random.Next(10) + 1;
+                            }
+                            Console.WriteLine("Sakumā int[] = " + String.Join(", ", intArray));
+                            Console.WriteLine("Parveidots int[] = " + String.Join(", ", Task1.GetOnlySmallValues(intArray)));
+                            break;
+                        }
+                    case "extra":
+                        {
+                            break;
+                        }
+                    case "exit":
+                        {
+                            check = true;
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Lūdzu evadiet pareizo ciparu vai vardu!!!");
+                            break;
+                        }
+                }
+            }
+        }
+    }
+}
